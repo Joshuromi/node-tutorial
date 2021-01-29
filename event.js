55d5e8e8a57d3678489d5396eae570b565dbbb53
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-01-29 20:05:11 +0100

    learnt to use event as a class and import as a module

--- event.js
+++ event.js
@@ -1,28 +1,9 @@
-const path = require("path");
-const os = require("os");
-const fs = require("fs");
-
-const EventEmitter = require("events");
-const emitter = new EventEmitter();
+const Logger = require("./logger");
+const logger = new Logger();
 
 //register a listener
-emitter.on("messageLogged", (eventArg) =>
+logger.on("messageLogged", (eventArg) =>
   console.log("Listener called", eventArg)
 );
 
-//raise an event
-emitter.emit("messageLogged", { id: 1, url: "http://" });
-
-const pathObj = path.parse(__filename);
-const totalMemory = os.totalmem();
-const freeMemory = os.freemem();
-
-// const files = fs.readdirSync("../");
-const files = fs.readdir("../", (err, file) => {
-  if (err) console.log("error", err);
-  else console.log("result", file);
-});
-
-console.log(pathObj);
-console.log(`Total memory: ${totalMemory}`);
-console.log(`Free memory: ${freeMemory}`);
+logger.log("message");
