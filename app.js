6b70ace37d91d455f952efdfaf9cb723e85f75f2
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-01-29 19:26:32 +0100

    learnt how to use path.parse() method

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+//register a listener
+emitter.on("messageLogged", () => console.log("Listener called"));
+
+//raise an event
+emitter.emit("messageLogged");
+
+const pathObj = path.parse(__filename);
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+// const files = fs.readdirSync("../");
+const files = fs.readdir("../", (err, file) => {
+  if (err) console.log("error", err);
+  else console.log("result", file);
+});
+
+console.log(pathObj);
+console.log(`Total memory: ${totalMemory}`);
+console.log(`Free memory: ${freeMemory}`);
